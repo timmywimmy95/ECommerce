b136506cbadfc3263698b79b6f07adf10c92de0c
timmywimmy <timothykwok95@gmail.com>
2023-05-02 18:25:16 +0800

    use session username for dashboard header

--- fleet_management/components/Header.js
+++ fleet_management/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { getSession, useSession } from 'next-auth/react';
 
 const Header = () => {
+  const { data: session } = useSession();
+
   return (
     <div className='flex justify-between px-4 pt-4 '>
-      <h2>Dashboard</h2> <h2>Welcome Back</h2>
+      <h2>Dashboard</h2>{' '}
+      <h2>Welcome Back {session ? session.user.username : <></>}</h2>
     </div>
   );
 };
